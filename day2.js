6f0539ad22e6aa8999f71653fc92127ca083a172
Vijay Prakash Sidar <vijayprakash.s@kevells.com>
2022-02-24 14:37:02 +0530

    day2 tutorials

---
+++ day2.js
@@ -0,0 +1,69 @@
+// 
+let myAge = 20;
+let country = 'China';
+let checkEquality = 1 === 1
+
+console.log(checkEquality);
+
+// return , break , continue
+
+function abc () {
+    return
+}
+
+// // loops
+// for(let i = 0; i <= 10; i++) {
+//     if(i === 5) {
+//         break;
+//     } else if (i < 4 && i > 1) {
+//         continue
+//     }
+//     console.log(i);
+    
+// }
+//
+// let evenNumberCOunt = 0;
+// for (let j = 0; j < 100; j++ ) {
+//     if(j % 2 === 0 ) {
+//         if (j === 0) {
+//             continue
+//         }
+//         console.log(j);
+//         if (evenNumberCOunt === 20) {
+//             break;
+//         }
+//         evenNumberCOunt++;
+//     }
+// }
+
+
+let person1 = {
+    'name' : 'Ram',
+    'age' : 24
+}
+
+let people = [];
+for( let i = 1; i <= 11; i ++) {
+    let dObj = {'name' : 'person' + i, 'age' : i * 10 }
+    people.push(dObj);
+}
+console.log(people)
+// people.length = 4
+// console.log(people);
+// people.shift() 0 1, ....n
+people.splice(3, 1)
+console.log(people)
+
+name , age, address, id
+
+persons = []
+personN = {
+    name : 'ANything',
+    age : 24,
+    address: 'Rando, Adrress',
+    id: 'uniqueId'
+}
+removePerson(10) {
+    pee
+}
+// 
